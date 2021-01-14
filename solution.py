3fd123b22c9106274ad9dfae51c6bfbeab558e61
Gurkirt Singh <GurkirtSingh143@gmail.com>
2021-01-13 21:10:34 -0800

    initial setup solution

---
+++ solution.py
@@ -0,0 +1,12 @@
+# "Maximize It" is a challenge on the HackerRank website.
+# link to the problam:- https://www.hackerrank.com/challenges/maximize-it/problem
+
+# Resources:-
+
+import math
+
+# Get the number of lists and devider from first line
+input = input().rstrip().split()
+num_of_list = input[0]
+devider = input[1] 
+
